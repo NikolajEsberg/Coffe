6039c211c16c89244fbe7e6f6d07dec4d7e147ae
MartinWagnerThomsen <marwagtho@gmail.com>
2025-10-02 11:18:33 +0200

    Martin03

--- src/Opgave/CoffeeMachineTest.java
+++ src/Opgave/CoffeeMachineTest.java
@@ -3,5 +3,19 @@ package Opgave;
 public class CoffeeMachineTest {
     public static void main(String[] args) {
 
+    switch(CoffeMachine) {
+        case "1" -> Coffee();
+        case "2" -> Espresso();
+        case "3" -> Double Espresso();
+        case "4" -> Triple Espresso();
+        case "5" -> Latte Machiatto();
+        case "6" -> ChocolateMilk();
+        case "7" -> ChocolateDrink();
     }
+    System.out.println("Your drink is served");
+
+
+
+
+
 }
