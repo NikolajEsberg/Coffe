87be3af405d0a9935e6999572cb483b4c2b539fc
Nikolaj Esberg <nikolajesberg@gmail.com>
2025-10-02 10:44:57 +0200

    Klasser Coffee og ChocolateDrink

--- src/Opgave/ChocolateDrink.java
+++ src/Opgave/ChocolateDrink.java
@@ -1,4 +1,15 @@
 package Opgave;
 
-public class ChocolateDrink {
+class ChocolateDrink extends Drink {
+    public ChocolateDrink(String name) {
+        super(name);
+    }
+    @Override
+    public int compareTo(Drink other) {
+        return this.name.compareToIgnoreCase(other.name);
+    }
+    @Override
+    public Cup serve(Size size, boolean whippedCream) {
+        return new Cup(this.name, size, true);
+    }
 }
--- src/Opgave/Coffee.java
+++ src/Opgave/Coffee.java
@@ -1,4 +1,15 @@
 package Opgave;
 
-public class Coffee {
+class Coffee extends Drink {
+    public Coffee(String name) {
+        super(name);
+    }
+    @Override
+    public int compareTo(Drink other) {
+        return this.name.compareToIgnoreCase(other.name);
+    }
+    @Override
+    public Cup serve(Size size, boolean whippedCream) {
+        return new Cup(this.name, size, false);
+    }
 }
