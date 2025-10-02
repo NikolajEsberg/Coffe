4c8b407d0176be9bbc7b12812c26bfedb1f18e79
AsiaAlGhazali <asiaalghazali@gmail.com>
2025-10-02 11:15:15 +0200

    commit drink.

--- src/Opgave/CoffeMachine.java
+++ src/Opgave/CoffeMachine.java
@@ -12,30 +12,31 @@ public class CoffeeMachine {
         drinks.add(new Espresso("Espresso"));
         drinks.add(new ChocolateDrink("Hot Chocolate"));
 
+    }
+        /*
+         * Serve en drik
+         * - Søger i listen efter navn
+         * - Kalder serve() på den rigtige drik
+         */
+        public Cup serveDrink (String drinkName, Size size,boolean whippedCream){
+            for (Drink drink : drinks) {
+                if (drink.getName().equalsIgnoreCase(drinkName)) {
+                    return drink.serve(size, whippedCream);
+                }
+            }
 
-    /*
-     * Serve en drik
-     * - Søger i listen efter navn
-     * - Kalder serve() på den rigtige drik
-     */
-    public Cup serveDrink(String drinkName, Size size, boolean whippedCream) {
-        for (Drink drink : drinks){
-            if (drink.getName().equalsIgnoreCase(drinkName)){
-                return drink.serve(size, whippedCream);
+            System.out.println("Drikken findes ikke:" + drinkName);
+            return null;
+        }
+
+        public void sortDrinks () {
+            drinks.sort(null);
+        }
+
+        public void listDrinks () {
+            for (Drink drink : drinks) {
+                System.out.println(drink);
             }
         }
-
-        System.out.println("Drikken findes ikke:" + drinkName);
-        return null;
     }
 
-    public void sortDrinks(){
-        drinks.sort(null);
-    }
-
-    public void listDrinks(){
-        for (Drink drink : drinks){
-            System.out.println(drink);
-        }
-    }
-}
